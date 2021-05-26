3960894b8bb5322629de442592ade43c9e5d53c9
Keith Filkins <kafilkins@me.com>
2021-05-26 14:11:08 -0400

    commit tree f7607e02981a55e12355789d107ae45c19e1ccc3

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -1,21 +1,31 @@
 class Person
   attr_accessor :partner, :name
-
+ 
   def initialize(name)
     @name = name
   end
-
+ 
   def get_married(person)
     self.partner = person
-    person.partner = self
+    if person.class != Person
+      begin
+        raise PartnerError
+      rescue PartnerError => error
+          puts error.message
+      end
+    else
+      person.partner = self
+    end
   end
-
+ 
+  class PartnerError < StandardError
+    def message
+      binding.pry
+      "#{beyonce.name} is married to beyonce.get_married."
+    end
+  end
 end
-
-Beyonce = Person.new("Beyonce")
+ 
+beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
 puts beyonce.name
-
-
-
-
